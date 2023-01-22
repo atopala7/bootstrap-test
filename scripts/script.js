b4be3a0dd818f0d2894d467dfaed31a1b5644864
atopala7 <atopala7@gmail.com>
2023-01-22 11:12:55 -0500

    Cleaned up the code

--- scripts/script.js
+++ scripts/script.js
@@ -26,67 +26,37 @@ function iterateOverTag(tag) {
 	return text;
 }
 
-function replaceInnerHTML(element, text) {
-	element.innerHTML = text;
-}
-
-function resetCrawl() {
-	const resetButton = document.getElementById("reset");
-	const crawl = document.getElementById("crawl");
-	const text = `<p>It is a period of civil war.
-		Rebel spaceships, striking
-		from a hidden base, have won
-		their first victory against
-		the evil Galactic Empire.
-		
-		During the battle, Rebel
-		spies managed to steal secret
-		plans to the Empire's
-		ultimate weapon, the DEATH
-		STAR, an armored space
-		station with enough power to
-		destroy an entire planet.
-		
-		Pursued by the Empire's
-		sinister agents, Princess
-		Leia races home aboard her
-		starship, custodian of the
-		stolen plans that can save
-		her people and restore
-		freedom to the galaxy....</p>`
-		
-	crawl.innerHTML = text;
-}
-
-function iterateOverLinks() {
-	iterateOverTag("a");
-}
-
-function jedi() {
-	const name = prompt("What is the Jedi's name?");
-	
-	let jediText = document.getElementById("jedi");
-	
-	jediText.innerHTML = name;
-}
-
-function sith() {
-	const name = prompt("What is the Sith's name?");
-	
-	let sithText = document.getElementById("sith");
-	
-	sithText.innerHTML = name;
-}
-
 function changeColor(button, color) {
 	return () => {
 		document.getElementById(button).style.backgroundColor = color;
 	};
 }
 
+function placeStars(element) {
+	// Place stars behind the element
+	const numStars = 454;
+	
+	// Remove any stars that already exist
+	for (let star of document.querySelectorAll(".star")) {
+		element.removeChild(star);
+	}
+
+	for (let i = 0; i < numStars; i++) {
+		let star = document.createElement("div");
+		let rect = element.getBoundingClientRect();
+		star.style.top = Math.floor(Math.random() * element.offsetHeight + rect.top) + "px";
+		star.style.left = Math.floor(Math.random() * element.offsetWidth) + "px";
+		star.style.position = "absolute";
+		star.style.width = "1px";
+		star.style.height = "1px";
+		star.style.backgroundColor = "white";
+		star.className = "star";
+		element.appendChild(star);
+	}
+}
+
 window.onload = () => {
-	document.getElementById("jedi").innerHTML="Jedi";
-	document.getElementById("sith").innerHTML="Sith";
+	let showingCrawl = true;
 	
 	const jedi = {
 		name: "Jedi",
@@ -99,6 +69,9 @@ window.onload = () => {
 		lightsaber: "Red",
 		element: document.querySelector("#sith")
 	}
+
+	jedi.element.innerHTML="Jedi";
+	sith.element.innerHTML="Sith";
 	
 	const jediColor = changeColor("jedi", jedi.lightsaber);
 	const sithColor = changeColor("sith", sith.lightsaber);
@@ -133,10 +106,26 @@ window.onload = () => {
 		freedom to the galaxy....\n<p>`
 
 	resetButton.addEventListener("click", () => {
+		showingCrawl = true;
 		crawl.innerHTML = text;
+		placeStars(crawl);
 	});
 	iterateButton.addEventListener("click", () => {
+		showingCrawl = false;
 		crawl.innerHTML = iterateOverTag("a");
+	});
+
+	placeStars(crawl);
+
+	[jedi, sith].forEach(obj => {
+		obj.element.addEventListener("mouseover", () => {
+			crawl.style.color = obj.lightsaber;
+			crawl.style.backgroundColor = "black";
+		});
+		obj.element.addEventListener("mouseout", () => {
+			crawl.style.backgroundColor = "black";
+			crawl.style.color = "#FFFF82";
+		});
 	});
 
 	// for (obj of [jedi, sith]) {
@@ -152,15 +141,11 @@ window.onload = () => {
 	// 	})(obj);
 	// }
 
-	[jedi, sith].forEach(obj => {
-		obj.element.addEventListener("mouseover", () => {
-			crawl.style.color = obj.lightsaber;
-			crawl.style.backgroundColor = "black";
-			console.log("obj is " + obj.name + ", with lightsaber " + obj.lightsaber + " and element " + obj.element.id);
-		});
-		obj.element.addEventListener("mouseout", () => {
-			crawl.style.backgroundColor = "black";
-			crawl.style.color = "#FFFF82";
-		});
-	});
+	// A listener to repopulate the stars whenever the window is resized
+	// Can be CPU-intensive without a delay function
+	// window.addEventListener("resize", () => {
+	// 	if (showingCrawl) {
+	// 		placeStars(crawl);
+	// 	}
+	// });
 }
